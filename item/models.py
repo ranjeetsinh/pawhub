4c6d852387741db5994e2244d0685dbaef57e020
ranjeetsinh <ranjeetsinhjagdale10@gmail.com>
2024-01-26 12:47:08 +0530

    Add new items to index page

--- item/models.py
+++ item/models.py
@@ -23,3 +23,6 @@ class Item(models.Model):
     is_adopted = models.BooleanField(default = False)
     created_by = models.ForeignKey(User, related_name = 'item', on_delete = models.CASCADE )
     created_at = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.name
