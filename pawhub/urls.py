74d0ffb8c767d33d10001703c96643ed69297cf1
ranjeetsinh <ranjeetsinhjagdale10@gmail.com>
2024-01-31 06:52:39 +0530

    Add urls file to core and include it in main urls file

--- pawhub/urls.py
+++ pawhub/urls.py
@@ -8,7 +8,6 @@ from core.views import index, contact
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
+    path('', include('core.urls')),
     path('items/', include('item.urls')),
-    path('contact/', contact, name= "contact"),
 ] + static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
